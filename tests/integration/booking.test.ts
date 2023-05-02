fbd76a8b4006ffd4f2c6f0a67767c31f0a4a96a5
Hugo Junior <hjunioreng@gmail.com>
2023-05-02 06:16:44 -0300

    feat  create test

--- tests/integration/booking.test.ts
+++ tests/integration/booking.test.ts
@@ -107,3 +107,169 @@ describe('GET /booking', () => {
     // });
   });
 });
+
+describe('POST /booking', () => {
+  it('should respond with status 401 if no token is given', async () => {
+    const response = await server.post('/booking');
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it('should respond with status 401 if given token is not valid', async () => {
+    const token = faker.lorem.word();
+
+    const response = await server.post('/booking').set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it('should respond with status 401 if there is no session for given token', async () => {
+    const userWithoutSession = await createUser();
+    const token = jwt.sign({ userId: userWithoutSession.id }, process.env.JWT_SECRET);
+
+    const response = await server.post('/booking').set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  describe('when token is valid', () => {
+    it('should respond with status 400 if query param ticketId is missing', async () => {
+      const token = await generateValidToken();
+
+      const response = await server.post('/booking').set('Authorization', `Bearer ${token}`);
+
+      expect(response.status).toEqual(httpStatus.NOT_FOUND);
+    });
+
+    it('should respond with status 404 when given ticket doesnt exist', async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      await createEnrollmentWithAddress(user);
+
+      const response = await server.post('/payments?ticketId=1').set('Authorization', `Bearer ${token}`);
+
+      expect(response.status).toEqual(httpStatus.NOT_FOUND);
+    });
+
+    // it('should respond with status 401 when user doesnt own given ticket', async () => {
+    //   const user = await createUser();
+    //   const token = await generateValidToken(user);
+    //   await createEnrollmentWithAddress(user);
+    //   const ticketType = await createTicketType();
+
+    //   const otherUser = await createUser();
+    //   const otherUserEnrollment = await createEnrollmentWithAddress(otherUser);
+    //   const ticket = await createTicket(otherUserEnrollment.id, ticketType.id, TicketStatus.RESERVED);
+
+    //   const response = await server.get(`/payments?ticketId=${ticket.id}`).set('Authorization', `Bearer ${token}`);
+
+    //   expect(response.status).toEqual(httpStatus.UNAUTHORIZED);
+    // });
+
+    // it('should respond with status 200 and with payment data', async () => {
+    //   const user = await createUser();
+    //   const token = await generateValidToken(user);
+    //   const enrollment = await createEnrollmentWithAddress(user);
+    //   const ticketType = await createTicketType();
+    //   const ticket = await createTicket(enrollment.id, ticketType.id, TicketStatus.RESERVED);
+
+    //   const payment = await createPayment(ticket.id, ticketType.price);
+
+    //   const response = await server.get(`/payments?ticketId=${ticket.id}`).set('Authorization', `Bearer ${token}`);
+
+    //   expect(response.status).toEqual(httpStatus.OK);
+    //   expect(response.body).toEqual({
+    //     id: expect.any(Number),
+    //     ticketId: ticket.id,
+    //     value: ticketType.price,
+    //     cardIssuer: payment.cardIssuer,
+    //     cardLastDigits: payment.cardLastDigits,
+    //     createdAt: expect.any(String),
+    //     updatedAt: expect.any(String),
+    //   });
+    // });
+  });
+});
+
+describe('PUT /booking', () => {
+  it('should respond with status 401 if no token is given', async () => {
+    const response = await server.put('/booking');
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it('should respond with status 401 if given token is not valid', async () => {
+    const token = faker.lorem.word();
+
+    const response = await server.put('/booking').set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it('should respond with status 401 if there is no session for given token', async () => {
+    const userWithoutSession = await createUser();
+    const token = jwt.sign({ userId: userWithoutSession.id }, process.env.JWT_SECRET);
+
+    const response = await server.put('/booking').set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  describe('when token is valid', () => {
+    it('should respond with status 400 if query param ticketId is missing', async () => {
+      const token = await generateValidToken();
+
+      const response = await server.put('/booking').set('Authorization', `Bearer ${token}`);
+
+      expect(response.status).toEqual(httpStatus.NOT_FOUND);
+    });
+
+    it('should respond with status 404 when given ticket doesnt exist', async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      await createEnrollmentWithAddress(user);
+
+      const response = await server.put('/payments?ticketId=1').set('Authorization', `Bearer ${token}`);
+
+      expect(response.status).toEqual(httpStatus.NOT_FOUND);
+    });
+
+    // it('should respond with status 401 when user doesnt own given ticket', async () => {
+    //   const user = await createUser();
+    //   const token = await generateValidToken(user);
+    //   await createEnrollmentWithAddress(user);
+    //   const ticketType = await createTicketType();
+
+    //   const otherUser = await createUser();
+    //   const otherUserEnrollment = await createEnrollmentWithAddress(otherUser);
+    //   const ticket = await createTicket(otherUserEnrollment.id, ticketType.id, TicketStatus.RESERVED);
+
+    //   const response = await server.get(`/payments?ticketId=${ticket.id}`).set('Authorization', `Bearer ${token}`);
+
+    //   expect(response.status).toEqual(httpStatus.UNAUTHORIZED);
+    // });
+
+    // it('should respond with status 200 and with payment data', async () => {
+    //   const user = await createUser();
+    //   const token = await generateValidToken(user);
+    //   const enrollment = await createEnrollmentWithAddress(user);
+    //   const ticketType = await createTicketType();
+    //   const ticket = await createTicket(enrollment.id, ticketType.id, TicketStatus.RESERVED);
+
+    //   const payment = await createPayment(ticket.id, ticketType.price);
+
+    //   const response = await server.get(`/payments?ticketId=${ticket.id}`).set('Authorization', `Bearer ${token}`);
+
+    //   expect(response.status).toEqual(httpStatus.OK);
+    //   expect(response.body).toEqual({
+    //     id: expect.any(Number),
+    //     ticketId: ticket.id,
+    //     value: ticketType.price,
+    //     cardIssuer: payment.cardIssuer,
+    //     cardLastDigits: payment.cardLastDigits,
+    //     createdAt: expect.any(String),
+    //     updatedAt: expect.any(String),
+    //   });
+    // });
+  });
+});
